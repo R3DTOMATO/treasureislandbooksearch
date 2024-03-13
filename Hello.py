8f01bce33c8dfcabae0eee258d052d1c7883b921
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2024-03-13 13:20:51 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -33,7 +33,7 @@ class MultiApp:
         app = option_menu(
         menu_title=None,
         options=["책명","작가"],
-        icons=["book","writing"],
+        icons=["book","✍"],
         menu_icon="cast",
         default_index=0,
         orientation="horizontal",
