e9458ed473ce4cc2285b4cd4757b41a8aa0d4659
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2024-03-13 11:18:57 +0900

    Update bookname.py

--- bookname.py
+++ bookname.py
@@ -5,13 +5,13 @@ from urllib.request import urlopen
 
 def app():
     def book():
-        url="https://drive.google.com/file/d/1BPSFw6-eP4IWbJe6CDXZ-Nx27Su9RQry/view?usp=sharing"
+        url="https://drive.google.com/file/d/1TcmkXyB4JQQJJhqwq87QbO3N9Y_isdPM/view?usp=sharing"
         response = urllib.request.urlopen(url)
         df = pd.read_csv("도서정보.csv")
         return df.set_index("책제목")
 
     try:
-        urllib.request.urlopen("https://drive.google.com/file/d/1BPSFw6-eP4IWbJe6CDXZ-Nx27Su9RQry/view?usp=sharing")
+        urllib.request.urlopen("https://drive.google.com/file/d/1TcmkXyB4JQQJJhqwq87QbO3N9Y_isdPM/view?usp=sharing")
         df = book()
         bookSelect = st.multiselect(
         label="책 이름을 입력하세요", options=list(df.index), default=["우리 집 늙은 고양이가 하는 말"]
