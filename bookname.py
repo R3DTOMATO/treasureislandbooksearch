9fcfbceec0360fd682cd83c54e6f6165ba36c4a6
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2024-03-13 12:01:24 +0900

    Update bookname.py

--- bookname.py
+++ bookname.py
@@ -7,7 +7,7 @@ def app():
     def book():
         url="https://drive.google.com/file/d/1qZKgawn4iYB6ADeUnWuu4s2Put7T4Ltt/view?usp=sharing"
         response = urllib.request.urlopen(url)
-        df = pd.read_csv("도서정보.csv")
+        df = pd.read_csv("도서정보.csv",encoding='cp949')
         return df.set_index("책제목")
 
     try:
